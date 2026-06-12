c4c63651050a92a2cc3a90e2158e1b5e00a2a7c9
Said Santos <98183325+said438@users.noreply.github.com>
2026-06-12 05:02:32 -0300

    validación de contactos

--- JS/contactos/formulario.js
+++ JS/contactos/formulario.js
@@ -1,124 +1,103 @@
 document.getElementById("btn-enviar").addEventListener("click", (event) => {
     event.preventDefault(); // -> Evita que se recargue la pagina al hacer click al boton "Enviar"
 
+    campoNombreCompletoEsValido();
+    campoCorreoEsValido();
+    campoTelefonoEsValido();
+    campoMensajeEsValido();
+});
 
-
-}
-
-function campoNombreCompletoEsValido(regex, input, mensajeDeError){
-    formatoEsValido(input, regex, mensajeDeError);
-
-    /*Hacer: No se puede ejecutar la funcion formatoDelCampoEsValido()
-    sin que campoEstaVacio() de false primero*/
-    const campoVacio = campoEstaVacio(input); //tiene que dar false
-    const formatoValido = formatoDelCampoEsValido(input, regex, mensajeDeError); //tiene que dar true
-    
-
-    if(formatoValido && campoVacio === false){
-        
-    }
-}
-
-function formatoDelCampoEsValido(input, regex, mensajeDeError){
+//Funciones de validación principales
+function campoNombreCompletoEsValido(){
+    //Regex: Valida un nombre completo.
+    const regex = /^[A-Z][a-z]{3,15} [A-Z][a-z]{3,15}$/; 
+    const input = document.getElementById("nombre-completo");
+    const mensajeDeError = `
+        Campo invalido: Ingrese solo el nombre, el apellido y presione el boton "Enviar" nuevamente.
+    `;
     const textoAValidar = input.value.trim();
 
-    if(regex.test(textoAValidar)){
-        ocultarErrorDeFormato(input);
-        return true
-    }else{
+    //Si el campo esta vacio
+    if(input.value.trim() === ""){
+        indicarQueElCampoEsObligatorio(input);
+        return false;
+    }
+
+    ocultarQueElCampoEsObligatorio(input);
+
+    //Si el formato del texto ingresado es invalido
+    if(!regex.test(textoAValidar)){
         mostrarErrorDeFormato(input, mensajeDeError);
         return false;
     }
-}
 
-function campoEstaVacio(input){
-    const textoAValidarEstaVacio = input.value.trim() === "";
-
-    if(textoAValidarEstaVacio){
-        mostrarIndicacionDeCampoObligatorio(input);
-        return false;
-    }
-
-    ocultarIndicacionDeCampoObligatorio(input);
+    ocultarErrorDeFormato(input);
     return true;
 }
 
-
-
-/*
-document.getElementById("btn-enviar").addEventListener("click", (event) => {
-    event.preventDefault(); // -> Evita que se recargue la pagina al hacer click al boton "Enviar"
-
-    //Validar si los campos estan Vacios
-    const camposValidos =
-    campoEsValido(
-        /^[A-Z][a-z]{3,15} [A-Z][a-z]{3,15}$/,
-        document.getElementById("nombre-completo"),
-        `Error: Ingrese solo el nombre y el apellido presione el boton "Enviar" nuevamente.`
-    );
-    campoEsValido(
-        //Regex: Valida un correo electronico.
-        /^[A-Za-z0-9_]{5,30}@(gmail|hotmail|outlook)\.(com|ar|net)$/,
-        document.getElementById("correo"),
-        `Error: Ingrese un correo valido y presione el boton "Enviar" nuevamente.`
-    );
-    campoEsValido(
-        //Regex: Valida que sea un numero de telefono de Argentina
-        /^\+549[0-9]{10}$/,
-        document.getElementById("telefono"),
-        `Error: Ingrese un numero de telefono valido para la republica Argentina(+54).`
-    );
-    campoEsValido(regex, input, mensajeDeError);
-
-    validarSiCampoEstaVacio(document.getElementById("nombre-completo"));
-    validarSiCampoEstaVacio(document.getElementById("correo"));
-    validarSiCampoEstaVacio(document.getElementById("telefono"));
-    validarSiCampoEstaVacio(document.getElementById("mensaje"));
-
-
-    console.log(campoVacio !== true);
-    
-        //Validar si hay error de formato en algun campo
-        validarFormatoDelCampo(
-            //Regex: Valida que se ingrese un nombre completo (sin incluir el segundo nombre).
-            
-        );
-
-        validarFormatoDelCampo(
-            
-        );
-
-        validarFormatoDelCampo(
-            
-        );
-    }
-
-    if(errorDeFormato === false || campoVacio === false){
-        const formulario = document.getElementById("formulario-contactos");
-        formulario.submit(); // -> Envia el formulario y recarga la pagina al cliquear el btn "Enviar"
-    }
-);
-
-function campoEsValido(regex, input, mensajeDeError){
+function campoCorreoEsValido(){
+    //Regex: Valida un correo electronico.
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const input = document.getElementById("correo");
+    const mensajeDeError = `
+        Campo invalido: Ingrese un correo valido y presione el boton "Enviar" nuevamente.
+    `;
     const textoAValidar = input.value.trim();
 
-    if(textoAValidar !== ""){
-        mostrarIndicacionCampoObligatorio(input);
+    //Si el campo esta vacio
+    if(input.value.trim() === ""){
+        indicarQueElCampoEsObligatorio(input);
         return false;
-    }else{
-        ocultarCampoObligatorio(input);
     }
 
-    if(regex.test(textoAValidar) === false){
+    ocultarQueElCampoEsObligatorio(input);
+
+    //Si el formato del texto ingresado es invalido
+    if(!regex.test(textoAValidar)){
         mostrarErrorDeFormato(input, mensajeDeError);
-        return true;
-    }else{
-        ocultarErrorDeFormato(input);
+        return false;
     }
-    
+
+    ocultarErrorDeFormato(input);
     return true;
 }
 
+//El campo de telefono es opcional
+function campoTelefonoEsValido(){
+    //Regex: Valida que solo se ingresen posibles numeros de telefono
+    const regex = /^(\+)?[0-9]+$/;
+    const input = document.getElementById("telefono");
+    const mensajeDeError = `
+        Campo invalido: Ingrese un numero de telefono valido. y presione el boton "Enviar" nuevamente.
+    `;
+    const textoAValidar = input.value.trim();
+
+    //Si el formato del texto ingresado es invalido
+    if(!regex.test(textoAValidar)){
+        mostrarErrorDeFormato(input, mensajeDeError);
+        return false;
+    }
+
+    ocultarErrorDeFormato(input);
+    return true;
+}
+
+function campoMensajeEsValido(){
+    //Regex: Valida un correo electronico.
+    const input = document.getElementById("mensaje");
+    const textoAValidar = input.value.trim();
+
+    //Si el campo esta vacio
+    if(input.value.trim() === ""){
+        indicarQueElCampoEsObligatorio(input);
+        return false;
+    }
+
+    ocultarQueElCampoEsObligatorio(input);
+    return true;
+}
+
+//Funciones auxiliares para enviarle feedback al usuario
 function mostrarErrorDeFormato(input, mensaje){
     const error = input.parentElement.querySelector(".mensaje-error");
     error.textContent = `${mensaje}`;
@@ -129,12 +108,12 @@ function ocultarErrorDeFormato(input){
     error.textContent = "";
 }
 
-function mostrarIndicacionCampoObligatorio(input){
+function indicarQueElCampoEsObligatorio(input){
     const error = input.parentElement.querySelector(".mensaje-error");
     error.textContent = "Este campo es obligatorio";
 }
 
-function ocultarIndicacionCampoObligatorio(input){
+function ocultarQueElCampoEsObligatorio(input){
     const error = input.parentElement.querySelector(".mensaje-error");
     error.textContent = "";  
 }
